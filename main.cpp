ce4e809b47b682bac1e8183e8408049ef0e7c0c7
Walcker Gomes <53241419+walcker@users.noreply.github.com>
2021-07-02 17:24:30 +0000

    Jogando os valores em um vetor

--- main.cpp
+++ main.cpp
@@ -14,17 +14,25 @@ int main(int argc, char *argv[]){
   //Permite usar acentos
     setlocale(LC_ALL,"");
 
-  int ic; //initial credit
+  /*int ic; //initial credit
   int nr; //number of rounds
-  int spots;// numeros apostados, máximo 15
+  int spots;// numeros apostados, máximo 15*/
+
+  string vetor[3];
 
   //imprime todos os argumentos recebidos na execução
   //do programa, para testar, compile e faça
   //./a.out arg1 arg2 arg2
   //o primeiro argumento é sempre o nome 
   //do programa (a.out, neste exemplo)
-  for(int i=0; i<argc; i++)
-    cout<<"Argumento["<<i<<"]: "<<argv[i]<<endl;
+  for(int i = 0; i < argc; i++){
+    cout << "Argumento[" <<i<< "]: " << argv[i] << endl;
+    vetor[i] = argv[i];
+  }
+
+  for(int i = 0; i <= 3; i++){
+    cout << vetor[i] << " " << endl;
+  }
 
   return 0;
 }
